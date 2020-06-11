593a211cbc944add96f4b2ba8442d56e934544e2
Vorák Gergő <vorakgeri@gmail.com>
2020-06-11 18:23:42 +0200

    form

---
+++ react-form/form/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../src/index.css';
+
+class Form extends React.Component
+{
+  render(){
+    return (
+      <form>
+
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(<Form />,document.getElementById('root'));
